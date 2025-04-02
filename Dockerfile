50496d958ae5c30405b70ce169e54ee45c39ab6d
Olg Muzalev <ruzrushitelyvselennoy@mail.ru>
2025-04-03 00:01:51 +0300

    done DZ 1 and 2

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,12 @@ FROM golang:1.23
 
 WORKDIR /app
 
-#ADD go.mod .
+ADD go.mod .
 
-#COPY . .
-COPY main.go .
+COPY . .
 
 RUN go build -o https-proxy main.go
 CMD ["./https-proxy"]
-#WORKDIR /
-
-#RUN go run *.go
-
-#FROM alpine
 
 EXPOSE 8080
+EXPOSE 8000
--- main.go
+++ main.go
@@ -20,112 +20,24 @@ const (
 	certsPath    = "./certs/ca_host.crt"
 )
 
-func requestsList(w http.ResponseWriter, r *http.Request) {
-	log.Println("RequestsList started")
-	htmlContent := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Requests List</title>
-			<meta charset="UTF-8">
-		</head>
-		<body>
-			<h1>Список запросов</h1>
-			<table border="1">
-				<tr>
-					<th>ID</th>
-					<th>Метод</th>
-					<th>Путь</th>
-					<th>Параметры</th>
-					<th>Заголовки</th>
-					<th>Cookie</th>
-					<th>Тело</th>
-				</tr>`
-	for i, req := range db.RequestInDB {
-		htmlContent += fmt.Sprintf(`
-			<tr>
-				<td><a href="/requests/%d">%d</a></td>
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-				<td>%s</td>
-			</tr>`, i, i, req.Method, req.Path, req.GetParams, req.Headers, req.Cookie, req.Body)
-	}
-	htmlContent += `
-			</table>
-		</body>
-		</html>`
-
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(htmlContent))
-}
-
-func requestByID(w http.ResponseWriter, r *http.Request) {
-	log.Println("requestByID started")
-	vars := mux.Vars(r)
-	id := vars["id"]
-	index := -1
-
-	fmt.Sscanf(id, "%d", &index)
-	if index < 0 || index >= len(db.RequestInDB) {
-		http.Error(w, "Invalid ID", http.StatusNotFound)
-		return
-	}
-
-	req := db.RequestInDB[index]
-	resp := db.ResponseInDB[index]
-
-	htmlContent := fmt.Sprintf(`
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Request Details</title>
-			<meta charset="UTF-8">
-		</head>
-		<body>
-			<h1>Запрос</h1>
-			<table border="1">
-				<tr><th>Метод</th><td>%s</td></tr>
-				<tr><th>Путь</th><td>%s</td></tr>
-				<tr><th>Параметры</th><td>%s</td></tr>
-				<tr><th>Заголовки</th><td>%s</td></tr>
-				<tr><th>Cookie</th><td>%s</td></tr>
-				<tr><th>Тело</th><td>%s</td></tr>
-			</table>
-			<h2>Ответ</h2>
-			<table border="1">
-				<tr><th>Статус</th><td>%s</td></tr>
-				<tr><th>Заголовки</th><td>%s</td></tr>
-				<tr><th>Тело</th><td>%s</td></tr>
-			</table>
-		</body>
-		</html>`,
-		req.Method, req.Path, req.GetParams, req.Headers, req.Cookie, req.Body,
-		resp.Method, resp.Headers, resp.Body)
-
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(htmlContent))
-}
-
 type InMemoryDB struct {
-	RequestInDB []Request
+	RequestInDB  []Request
 	ResponseInDB []Request
 }
 
 type Request struct {
-	Method string
-	Path string
+	Method    string
+	Path      string
 	GetParams string
-	Headers string
-	Cookie string
-	Body string
+	Headers   string
+	Cookie    string
+	Body      string
 }
 
-var db  = InMemoryDB{}
+var db = InMemoryDB{}
 
 func main() {
+	//сервер для раздачи произведенных запросов
 	info_server := mux.NewRouter()
 	info_server.HandleFunc("/requests", requestsList).Methods("GET")
 	info_server.HandleFunc("/requests/{id}", requestByID).Methods("GET")
@@ -137,7 +49,7 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
-
+    //сервер для принятия запросов на прокси
 	ln, err := net.Listen("tcp", proxyAddress)
 	if err != nil {
 		log.Fatalf("Failed to listen on %s: %v", proxyAddress, err)
@@ -155,6 +67,7 @@ func main() {
 	}
 }
 
+//универсальная часть соединения считывающая и изменяющая запрос и передающая его нужному обработчику
 func handleConnection(clientConn net.Conn) {
 	defer clientConn.Close()
 	buf := make([]byte, 8000)
@@ -177,10 +90,11 @@ func handleConnection(clientConn net.Conn) {
 	if strings.HasPrefix(lines[0], "CONNECT ") {
 		handleHTTPSConnection(clientConn, lines[0])
 	} else {
-		handleHTTPConnection(clientConn, buf[:n], lines)
+		handleHTTPConnection(clientConn, buf[:n], lines, port)
 	}
 }
 
+// убирает Proxy-Connection узнает host и port и при необходимости меняет путь на относительный
 func cutting(lines []string) ([]string, string, string) {
 	var result []string
 	port := "80"
@@ -207,6 +121,7 @@ func cutting(lines []string) ([]string, string, string) {
 	return result, host, port
 }
 
+// обрабатывает https соединение
 func handleHTTPSConnection(clientConn net.Conn, connectRequest string) {
 	log.Println("HTTPS connection:", connectRequest)
 	fields := strings.Split(connectRequest, " ")
@@ -252,7 +167,8 @@ func handleHTTPSConnection(clientConn net.Conn, connectRequest string) {
 	io.Copy(tlsClientConn, realServerConn)
 }
 
-func handleHTTPConnection(clientConn net.Conn, request []byte, lines []string) {
+// обрабатывает http соединение
+func handleHTTPConnection(clientConn net.Conn, request []byte, lines []string, port string) {
 	log.Println("HTTP connection:", lines)
 	var host string
 	for _, line := range lines {
@@ -267,7 +183,7 @@ func handleHTTPConnection(clientConn net.Conn, request []byte, lines []string) {
 		return
 	}
 
-	realServerConn, err := net.Dial("tcp", host+":80")
+	realServerConn, err := net.Dial("tcp", host+":"+port)
 	if err != nil {
 		log.Println("Failed to connect to real server:", err)
 		return
@@ -280,26 +196,97 @@ func handleHTTPConnection(clientConn net.Conn, request []byte, lines []string) {
 		return
 	}
 
-	// Записываем запрос в БД
-	db.RequestInDB = append(db.RequestInDB, Request{
-		Method:  lines[0],
-		Path:    host,
-		Headers: strings.Join(lines, "\n"),
-		Body:    string(request),
-	})
+	go io.Copy(realServerConn, clientConn)
+	io.Copy(clientConn, realServerConn)
+}
 
-	responseBuf, err := io.ReadAll(realServerConn)
-	if err != nil {
-		log.Println("Failed to read response from server:", err)
+// handler для вывода списка произведенных запросов (еще не доделан - заготовка для 3 дз)
+func requestsList(w http.ResponseWriter, r *http.Request) {
+	log.Println("RequestsList started")
+	htmlContent := `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Requests List</title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<h1>Список запросов</h1>
+			<table border="1">
+				<tr>
+					<th>ID</th>
+					<th>Метод</th>
+					<th>Путь</th>
+					<th>Параметры</th>
+					<th>Заголовки</th>
+					<th>Cookie</th>
+					<th>Тело</th>
+				</tr>`
+	for i, req := range db.RequestInDB {
+		htmlContent += fmt.Sprintf(`
+			<tr>
+				<td><a href="/requests/%d">%d</a></td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+				<td>%s</td>
+			</tr>`, i, i, req.Method, req.Path, req.GetParams, req.Headers, req.Cookie, req.Body)
+	}
+	htmlContent += `
+			</table>
+		</body>
+		</html>`
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(htmlContent))
+}
+
+// handler для вывода запроса (еще не доделан - заготовка для 3 дз)
+func requestByID(w http.ResponseWriter, r *http.Request) {
+	log.Println("requestByID started")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	index := -1
+
+	fmt.Sscanf(id, "%d", &index)
+	if index < 0 || index >= len(db.RequestInDB) {
+		http.Error(w, "Invalid ID", http.StatusNotFound)
 		return
 	}
 
-	// Записываем ответ в БД
-	db.ResponseInDB = append(db.ResponseInDB, Request{
-		Method: "HTTP Response",
-		Body:   string(responseBuf),
-	})
+	req := db.RequestInDB[index]
+	resp := db.ResponseInDB[index]
 
-	go io.Copy(realServerConn, clientConn)
-	io.Copy(clientConn, realServerConn)
+	htmlContent := fmt.Sprintf(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Request Details</title>
+			<meta charset="UTF-8">
+		</head>
+		<body>
+			<h1>Запрос</h1>
+			<table border="1">
+				<tr><th>Метод</th><td>%s</td></tr>
+				<tr><th>Путь</th><td>%s</td></tr>
+				<tr><th>Параметры</th><td>%s</td></tr>
+				<tr><th>Заголовки</th><td>%s</td></tr>
+				<tr><th>Cookie</th><td>%s</td></tr>
+				<tr><th>Тело</th><td>%s</td></tr>
+			</table>
+			<h2>Ответ</h2>
+			<table border="1">
+				<tr><th>Статус</th><td>%s</td></tr>
+				<tr><th>Заголовки</th><td>%s</td></tr>
+				<tr><th>Тело</th><td>%s</td></tr>
+			</table>
+		</body>
+		</html>`,
+		req.Method, req.Path, req.GetParams, req.Headers, req.Cookie, req.Body,
+		resp.Method, resp.Headers, resp.Body)
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(htmlContent))
 }
